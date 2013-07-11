d919e058536b10543922e2c8ca4b26189ebd7829
Jan Vansteenkiste <jan@vstone.eu>
2013-07-11 11:02:11 +0200

    Improved the connect irssi script.

--- irssi/connect-irssi.sh
+++ irssi/connect-irssi.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #
 
-REMOTE="moose.irken.org"
+port="${1-22}"
+host="${2-YOUR_HOSTNAME.example.com}"
+user="${3-USERNAME}"
+screen_name="${4-irssi}"
 
-socat -d -d -d -lf/tmp/socat.log -L/tmp/socat-irssi.lock -u \
-  TCP-LISTEN:12000,reuseaddr,fork,bind=127.0.0.1,tcpwrap=script \
-  EXEC:/home/jan/.local/bin/notify-remote 2>&1 >/dev/null &
+notify="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/notify-remote"
 
-Terminal -T irssi@moose --maximize -e "ssh -t -l jan -R 12000:127.0.0.1:12000 ${REMOTE} screen -d -r -s irssi"
-
-
-## When not using terminal, uncomment the kill %1 to kill socat when closing your connection down. I keep it open until I shutdown :)
-#kill %1
+socat -u tcp4-listen:12000,reuseaddr,fork,bind=127.0.0.1 exec:$notify &
+ssh $host -p $port -t -l $user -R 12000:127.0.0.1:12000 ${REMOTE} "screen -d -r -s ${screen_name}"
+kill %1
