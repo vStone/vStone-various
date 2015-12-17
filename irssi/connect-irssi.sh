d2ab67b31e0ebf332b5b60d2c07aa25b5ead2f28
Jan Vansteenkiste <jan@vstone.eu>
2015-12-17 09:08:13 +0100

    fix connect irssi script

--- irssi/connect-irssi.sh
+++ irssi/connect-irssi.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#
 
 port="${1-22}"
 host="${2-YOUR_HOSTNAME.example.com}"
@@ -8,6 +7,7 @@ screen_name="${4-irssi}"
 
 notify="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/notify-remote"
 
+set -e
 socat -u tcp4-listen:12000,reuseaddr,fork,bind=127.0.0.1 exec:$notify &
-ssh $host -p $port -t -l $user -R 12000:127.0.0.1:12000 ${REMOTE} "screen -d -r -s ${screen_name}"
+ssh $host -p $port -t -l $user -R 12000:127.0.0.1:12000 ${REMOTE} "screen -d -R -S ${screen_name} ${screen_name}"
 kill %1
